801f3a8f24ae075a40aed768ff91b3668076d0d1
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-12 12:53:03 +0300

    home page path added

--- pics/views.py
+++ pics/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Picture
 
 # Create your views here.
+
+def posted_pics(request):
+    
+    pics = Picture.objects.all()
+    
+    return render(request, 'pic/index.html', {"pics":pics})
