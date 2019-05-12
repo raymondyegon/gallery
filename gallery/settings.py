4aacf866d29fefa10eda686975ab5f67018cf459
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-12 12:49:03 +0300

    imagekit added

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pics'
+    'pics',
+    'bootstrap4',
+    'imagekit'
 ]
 
 MIDDLEWARE = [
