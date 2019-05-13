682397baa8f681004805574d48ccf7386511a829
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-13 16:18:19 +0300

    static path added

--- pics/urls.py
+++ pics/urls.py
@@ -7,4 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.posted_pics, name='Home'),
     path('search/', views.search_results, name='search_results')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
