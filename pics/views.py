eacf130f2edffe372aaa10b05830c7fff00b16c2
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-13 10:16:05 +0300

    Search functionality added

--- pics/views.py
+++ pics/views.py
@@ -9,3 +9,15 @@ def posted_pics(request):
     pics = Picture.objects.all()
     
     return render(request, 'pic/index.html', {"pics":pics})
+
+def search_results(request):
+    if 'pictures' in request.GET and request.GET["pictures"]:
+        searched_term = request.GET.get("pictures")
+        searched_pictures = Picture.search_by_title(searched_term)
+        message = f"{searched_term}"
+        
+        return render(request, 'pic/search.html', {"message":message, "pictures":searched_pictures})
+    
+    else:
+        message = "You have not searched for any picture"
+        return render(request, 'pic/search.html', {"message":message})
